3b40458d7e1a34878e7e93aa9b863e8ec14bd913
Apurv Parikh <apurvparikh1@gmail.com>
2015-06-09 16:31:41 -0400

    unauth add to cart

--- browser/js/store/store.js
+++ browser/js/store/store.js
@@ -24,17 +24,34 @@ app.controller('StoreSingleCtrl', function ($scope, $state, StoreFCT, $statePara
         });
 });
 
-app.controller('StoreCtrl', function ($scope, AuthService, $state, StoreFCT) {
+app.controller('StoreCtrl', function ($scope, AuthService, $state, StoreFCT, $localStorage) {
+
+    var cartData = [];
 
     StoreFCT.getAll().then(function (data) {
-        console.log('DATA on controller', data.data);
+        // console.log('DATA on controller', data.data);
         // $scope.products = data.data;
         $scope.products = data.data.map(function (obj) {
             obj.layerNum = obj.layers.length;
             obj.reviewNum = obj.reviews.length;
             return obj;
         });
-            console.log('OBJ', $scope.products);
-
+            // console.log('OBJ', $scope.products);
     });
+
+    $scope.addToCart = function (cake) {
+
+        // we will need a condition here to handle authenticated users
+
+        if ($localStorage.cart) {
+            cartData = $localStorage.cart;
+        }
+
+        cartData.push(cake);
+
+        $localStorage.cart = cartData;
+    } 
+
+
+
 });
