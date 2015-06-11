48df203970b8119026e3fc1459412c6fe00b06f1
Siddharth Joshi <drumfiend21@gmail.com>
2015-06-10 20:05:29 -0400

    cake build price generation functional

--- browser/js/common/directives/cake-builder-form/cake-builder-form.js
+++ browser/js/common/directives/cake-builder-form/cake-builder-form.js
@@ -1,4 +1,4 @@
-app.directive('buildForm', function (CakeFactory) {
+app.directive('buildForm', function (CakeFactory, $localStorage) {
 
     return {
         restrict: 'E',
@@ -8,20 +8,40 @@ app.directive('buildForm', function (CakeFactory) {
 
                 CakeFactory.getAllIngredients().then(function(ingredients){
 
+                    
+
                     //make ingredients available on scope
                     scope.fillings = ingredients.data[0]
                     scope.icings = ingredients.data[1]
                     scope.shapes = ingredients.data[2]
                     scope.reviews = ingredients.data[4]
 
-                    //****** TO BE DONE: PERSIST CAKE OBJECT BETWEEN FEATURE SELECTIONS *******
-                    
-
                     //build the cake object
                     scope.cake = {
 
-                        layers: [{ position: 1},{position: 2},{ position: 3}]
+                        name: null,
+                        type: "custom",
+                        price: 0,
+                        shape: null,
+                        icing: null,
+                        layers: [{ position: 1, filling: null}
+                        ,{position: 2, filling: null},{ position: 3, filling: null}]
+                        ,
+                        reviews: null,
+                    }
 
+                    if(!$localStorage.cake){
+                        $localStorage.cake = {
+                            name: null,
+                            type: "custom",
+                            price: 0,
+                            shape: null,
+                            icing: null,
+                            layers: [{ position: 1, filling: null}
+                            ,{position: 2, filling: null},{ position: 3, filling: null}]
+                            ,
+                            reviews: null,
+                        };
                     }
 
                     //to update cake object properties
@@ -30,8 +50,25 @@ app.directive('buildForm', function (CakeFactory) {
                         if(prop !== "filling") scope.cake[prop] = selected;
                         console.log(scope.cake)
 
+                        //check for layers desired and modify scope.cake.layers
+                        if(scope.cake["selectedNumLayers"]=== 1) {
+                            if(scope.cake.layers[1].filling !== null){
+                                scope.cake.layers[1].filling = null;
+                            }
+                            if(scope.cake.layers[2].filling !== null){
+                                scope.cake.layers[2].filling = null;
+                            }
+                            angular.element(layerTwo).css("display","none")
+                            angular.element(layerThree).css("display","none")
+                        }
+
                         if(scope.cake["selectedNumLayers"]=== 2) {
+                            if(scope.cake.layers[2].filling !== null){
+                                scope.cake.layers[2].filling = null;
+                            }
                             angular.element(layerTwo).css("display","block")
+                            angular.element(layerThree).css("display","none")
+                            
                         }
                         if(scope.cake["selectedNumLayers"]=== 3) {
                             angular.element(layerTwo).css("display","block")
@@ -39,6 +76,34 @@ app.directive('buildForm', function (CakeFactory) {
      
                         }
 
+                        //update localStorage.cake when scope.cake updates
+                        scope.setCakeLocal = function(cake){
+                            for(var key in cake){
+                                $localStorage.cake.key = cake.key
+                            }
+                        }
+                        scope.setCakeLocal(scope.cake)
+
+                        //update price of cake
+                        scope.priceUpdate = function(){
+
+                            scope.cake.price = 0
+
+                            if(scope.cake.icing !== null){
+                                scope.cake.price += scope.cake.icing.price
+                            }
+
+                            for(var i=0; i < 3; i++){
+                                
+                                if(scope.cake.layers[i].filling !== null){
+                                    scope.cake.price += scope.cake.layers[i].filling.price
+                                }
+
+                            }
+                            console.log(scope.cake.price)
+                        }
+                        scope.priceUpdate();
+                        
                     }
 
                     //for selecting the property to update w update function
@@ -47,8 +112,20 @@ app.directive('buildForm', function (CakeFactory) {
                     scope.icing = "icing"
                     scope.shape = "shape"
                     scope.numLayers = [1,2,3]
+ 
+                    
+                    //bring storeCake function to scope
+                    scope.storeCake = CakeFactory.storeCake
+
+                    //persist cake in progress from local storage
+                    scope.loadCakeFromLocal = function (){
+                        scope.cake = $localStorage.cake;
+                        console.log("loaded cake from localStorage",scope.cake)                       
+                    }
+                    scope.loadCakeFromLocal();
 
                 });
+
         }
 
     };
--- browser/js/common/factories/CakeFactory.js
+++ browser/js/common/factories/CakeFactory.js
@@ -1,12 +1,28 @@
-app.factory('CakeFactory', function ($http) {
+app.factory('CakeFactory', function ($http, $localStorage) {
 
     return {
         getAllIngredients: function () {
             return $http.get('/api/cake_builder').then(function(ingredients){
-                console.log("get ingredients hit from front end")
+                // console.log("get ingredients hit from front end")
                 return ingredients
             });
+        },
+        storeCake: function (cakeObj){
+        	return $http.post('/api/cake_builder', cakeObj).then(function(cake){
+        		// console.log("cake sent to database");
+        		// console.log(cake)
+        		delete $localStorage.cake
+        		return cake
+        	});
         }
+        // ,
+        // deleteCake: function (cakeName){
+        // 	console.log("hit the deleteCake", cakeName)
+        // 	return $http.delete('/api/cake_builder/'+cakeName).then(function(cake){
+        // 		console.log("cake delete complete on front end")
+        // 		return cake
+        // 	})
+        // }
     };
 
 });
