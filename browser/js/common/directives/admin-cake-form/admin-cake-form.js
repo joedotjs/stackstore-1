f8273726e9ffc6da4d2be4eac7b7cb63037f378e
Joseph Alves <databentpx@gmail.com>
2015-06-15 11:03:55 -0400

    Deleted

--- browser/js/common/directives/admin-cake-form/admin-cake-form.js
+++
@@ -1,6 +0,0 @@
-app.directive('adminCakeForm', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/common/directives/admin-cake-form/admin-cake-form.html'
-    };
-});
--- browser/js/common/directives/admin-item-form/admin-item-form.js
+++
@@ -1,6 +0,0 @@
-app.directive('adminItemForm', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/common/directives/admin-item-form/admin-item-form.html'
-    };
-});
